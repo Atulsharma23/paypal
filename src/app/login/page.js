78b79a5e7648ae793ed95e16a7bd6ebf7c533eab
atul <satul7003@gmail.com>
2023-09-04 17:16:41 +0530

    kbh

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,30 @@
+"use client";
+import { useEffect, useState } from "react";
 import Link from "next/link";
-export default function login() {
+
+export default function Login() {
+  const [showPass, setShowPass] = useState(false);
+  const [showConfirmPass, setShowConfirmPass] = useState(false);
+
+  useEffect((e) => {}, []);
+
+  function showPassfn(e) {
+    e.preventDefault();
+    console.log("first");
+    setShowPass(!showPass);
+  }
+
+  function showPassConfn(e) {
+    e.preventDefault();
+    console.log("first");
+    setShowConfirmPass(!showConfirmPass);
+  }
+
   return (
     <div className="Loginpage">
       <div className="container">
         <div className="left-section">
-          <img src="images/payken 1.png"></img>
+          <img src="images/payken 1.png" alt="Payken Logo" />
           <div className="left-des">
             <h2>Don't swap just Payken</h2>
             <h4>Welcome to Payken</h4>
@@ -14,21 +34,20 @@ export default function login() {
           <h3>Create Account</h3>
           <div className="divoisionkabaap">
             <div className="divisonof">
-              <label for="username"></label>
-
-              <img src="images/User.png" />
+              <label htmlFor="username"></label>
+              <img src="images/User.png" alt="Username Icon" />
               <input
                 type="text"
                 id="username"
                 name="username"
-                placeholder="username"
+                placeholder="Username"
                 required
               />
             </div>
             <br />
             <div className="divisonof">
-              <label for="Email"></label>
-              <img src="images/Email.png" />
+              <label htmlFor="Email"></label>
+              <img src="images/Email.png" alt="Email Icon" />
               <input
                 type="text"
                 id="Email"
@@ -40,42 +59,51 @@ export default function login() {
 
             <br />
             <div className="divisonof">
-              <label for="Password"></label>
-              <img src="images/Password.png" />
+              <label htmlFor="Password"></label>
+              <img src="images/Password.png" alt="Password Icon" />
               <input
-                type="Password"
+                type={showPass ? "text" : "password"}
                 id="Password"
                 name="Password"
                 placeholder="Password"
                 required
               />
-              <img className="eye-image" src="images/Eye.png" />
+              <img
+                className="eye-image"
+                src="images/Eye.png"
+                alt="Show Password"
+                onClick={showPassfn}
+              />
             </div>
 
             <br />
             <div className="divisonof">
-              <label for="username"></label>
-              <img src="images/Password.png" />
+              <label htmlFor="Confirm Password"></label>
+              <img src="images/Password.png" alt="Confirm Password Icon" />
               <input
-                type="Password"
+                type={showConfirmPass ? "text" : "password"}
                 id="Confirm Password"
                 name="Confirm Password"
                 placeholder="Confirm Password"
                 required
               />
-              <img className="eye-image" src="images/Eye.png" />
+              <img
+                className="eye-image"
+                src="images/Eye.png"
+                alt="Show Password"
+                onClick={showPassConfn}
+              />
             </div>
 
             <br />
             <button type="button" className="log">
               Login
             </button>
-            <br/>
+            <br />
             <div className="Newtopaypal">
-            <h6>Already Register?</h6><Link  href="/login">
-                LogIn
-              </Link>
-              </div>
+              <h6>Already Registered?</h6>
+              <Link href="/user">Log In</Link>
+            </div>
           </div>
         </div>
       </div>
